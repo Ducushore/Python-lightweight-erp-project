940eddcdcc7af61fde38a1f35ad9285214c8b6b8
math k999 <math.k999@gmail.com>
2016-12-13 11:31:19 +0100

    ui/get_inputs/v1.0

--- ui.py
+++ ui.py
@@ -86,9 +86,13 @@ def get_inputs(list_labels, title):
         List of data given by the user. Sample return:
             [<user_input_1>, <user_input_2>, <user_input_3>]
     """
+
     inputs = []
 
-    # your code
+    print(title)
+    for element in list_labels:
+        x = input(element)
+        inputs.append(x)
 
     return inputs
 
